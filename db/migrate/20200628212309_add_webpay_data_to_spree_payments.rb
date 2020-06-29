f7b56d7461564bfe067b8c3f537c3980f9d11c8b
sbstn-jmnz <sbstn.jmnz@yahoo.com>
2020-06-28 22:16:31 -0400

    commented hstore

--- db/migrate/20200628212309_add_webpay_data_to_spree_payments.rb
+++ db/migrate/20200628212309_add_webpay_data_to_spree_payments.rb
@@ -1,12 +1,12 @@
 class AddWebpayDataToSpreePayments < SpreeExtension::Migration
   def up
     execute 'CREATE EXTENSION IF NOT EXISTS hstore'
-    add_column :spree_payments, :webpay_params, :hstore
+    #add_column :spree_payments, :webpay_params, :hstore
     add_column :spree_payments, :webpay_trx_id, :string
   end
 
   def down
-    remove_column :spree_payments, :webpay_params
+    #remove_column :spree_payments, :webpay_params
     remove_column :spree_payments, :webpay_trx_id
     execute 'DROP EXTENSION hstore'
   end
