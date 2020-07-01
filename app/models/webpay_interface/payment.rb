e852f4c9c47bda164b198d2f404c021e967d7614
sbstn-jmnz <sbstn.jmnz@yahoo.com>
2020-06-30 23:45:50 -0400

    update comments in payment model

--- app/models/webpay_interface/payment.rb
+++ app/models/webpay_interface/payment.rb
@@ -10,10 +10,11 @@ module WebpayInterface
     # tbk_order_id - integer - The purchase order id.
     # session_id - integer - The user session id.
     #
-    # Returns the following object:
-    # { "token" => token.to_s,
-    #   "url" => url.to_s,
-    #   "error_desc" => "TRX_OK" 
+    # Returns a hash like:
+    # {
+    #  "token"=>"e38c6e07ccae238653bc6c78d23faaa670f83b0273430877828e7f237470e1ff",
+    #  "url"=>"https://webpay3gint.transbank.cl/webpayserver/initTransaction",
+    #  "error_desc"=>"TRX_OK"
     # }
     def init_normal_transaction tbk_total_price, order_id, session_id
       amount = tbk_total_price 
