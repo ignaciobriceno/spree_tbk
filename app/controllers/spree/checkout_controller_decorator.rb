912e16a567466889d0a02907bb5d1af342c5c9a4
sbstn-jmnz <sbstn.jmnz@yahoo.com>
2020-07-05 14:26:49 -0400

    allow to work with coonfirmation step during checkout

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -3,13 +3,13 @@ module Spree
     def update
       if payment_valid_params? && paying_with_webpay?
         provider       = payment_method.provider.new
-        amount         = @order.webpay_amount
+        amount         = @order.total.to_i
         init_trx = provider.init_normal_transaction(amount, @order.number, session.id)
         if init_trx["error_desc"] == "TRX_OK"
           response = Net::HTTP.post_form(URI(init_trx["url"]), token_ws: init_trx["token"])
           render html: response.body.html_safe
         else
-         render :edit 
+          render :edit 
         end
       else
         super       
@@ -27,12 +27,17 @@ module Spree
     end
 
     def payment_method_id
-      params[:order][:payments_attributes].first[:payment_method_id]
+      @order.payments.first.payment_method_id
     end
 
     def payment_valid_params?
-      params[:state] == 'payment' and @order.state == 'payment'
+      if @order.confirmation_required?
+        params[:state] == 'confirm' and @order.state == 'confirm'
+      else
+        params[:state] == 'payment' and @order.state == 'payment'
+      end
     end
+    
   end
 end
 
