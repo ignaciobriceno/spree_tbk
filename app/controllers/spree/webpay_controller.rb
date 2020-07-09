6cd9c3c6407117eea1a90bcac086c45024a0c7a2
ignaciobriceno <ignacioob.10@gmail.com>
2020-07-09 01:26:15 -0400

    added changes in checkout controller decorator

--- app/controllers/spree/webpay_controller.rb
+++ app/controllers/spree/webpay_controller.rb
@@ -5,6 +5,48 @@ module Spree
     
     def webpay_return_url
       trx_result = ''
+      if @result_trx['error_desc'] = 'TRX_OK'
+        @order = Spree::Order.find_by_number(@result_trx['buyorder'])
+        @payment = @order.payments.last
+        @payment.update(webpay_trx_id: params['token_ws'])
+        @payment.update(state: 'pending')
+        
+        state = @result_trx['error_desc'].to_s
+        accountingdate    = @result_trx['accountingdate'].to_s
+        buyorder          = @result_trx['buyorder'].to_s
+        sharesnumber      = @result_trx['sharesnumber'].to_s
+        cardnumber        = @result_trx['cardnumber'].to_s
+        amount            = @result_trx['amount'].to_s
+        commercecode      = @result_trx['commercecode'].to_s
+        authorizationcode = @result_trx['authorizationcode'].to_s
+        paymenttypecode   = @result_trx['paymenttypecode'].to_s
+        responsecode      = @result_trx['responsecode'].to_s
+        transactiondate   = @result_trx['transactiondate'].to_s
+        urlredirection    = @result_trx['urlredirection'].to_s
+        vci               = @result_trx['vci'].to_s
+
+        if responsecode == '0' 
+          webpay_data = [state, accountingdate, cardnumber, amount, authorizationcode, paymenttypecode, responsecode, transactiondate, urlredirection, vci, sharesnumber].to_s
+          @payment.update(state: 'complete')
+          @payment.update(webpay_params: webpay_data)
+          @order.update(completed_at: Time.now)
+          #@payment.update(pay_date: Time.now)
+          #@payment.update(verified: true)  
+          response = Net::HTTP.post_form(URI(urlredirection.to_s), token_ws: params[:token_ws])
+          flash[:order_completed] = true
+          render 'spree/orders/show'
+
+        else
+          @payment.update(webpay_params: @result_trx['error_desc'])
+          @payment.update(description: "Pago con WebPay de: "+ current_user.client.full_name + " con errores")
+          redirect_to webpay_error_path           
+        end
+        return          
+      else
+        @payment.update(description: "Pago con WebPay de: "+ current_user.client.full_name + " con errores")
+        @payment.destroy
+        redirect_to webpay_nullify_path
+      end    
     end
     
     def webpay_final_url
@@ -13,9 +55,8 @@ module Spree
 
     private
     def load_data
-      debugger
-      PaymentMethod.find_by_name('TransbankWebpay')
-      @payment = Spree::Payment.find_by(params["token_ws"])
+      provider = PaymentMethod.find_by_type('Spree::Gateway::SpreeTbk').provider.new
+      @result_trx = provider.transaction_result(params[:token_ws])
     end
     
     def completion_route
