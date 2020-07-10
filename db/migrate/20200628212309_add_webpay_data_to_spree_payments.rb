e07e35289140c98a14ce4129c62eed23739d9e94
ignaciobriceno <ignacioob.10@gmail.com>
2020-07-09 23:57:50 -0400

    added columns to migration

--- db/migrate/20200628212309_add_webpay_data_to_spree_payments.rb
+++ db/migrate/20200628212309_add_webpay_data_to_spree_payments.rb
@@ -1,13 +1,13 @@
-class AddWebpayDataToSpreePayments < SpreeExtension::Migration
+class AddWebpayDataToSpreePayments < SpreeExtension::Migration[4.2]
   def up
-    #execute 'CREATE EXTENSION IF NOT EXISTS hstore'
-    #add_column :spree_payments, :webpay_params, :hstore
+    execute 'CREATE EXTENSION IF NOT EXISTS hstore'
+    add_column :spree_payments, :webpay_params, :hstore
     add_column :spree_payments, :webpay_trx_id, :string
   end
 
   def down
-    #remove_column :spree_payments, :webpay_params
+    remove_column :spree_payments, :webpay_params
     remove_column :spree_payments, :webpay_trx_id
-    #execute 'DROP EXTENSION hstore'
+    execute 'DROP EXTENSION hstore'
   end
 end
