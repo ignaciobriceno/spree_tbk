eb77bdb914ef1cf44237c09c6a4be3e96deaf80c
ignaciobriceno <ignacioob.10@gmail.com>
2020-07-10 02:37:34 -0400

    hstore extension for postgresql

--- db/migrate/20200628212309_add_webpay_data_to_spree_payments.rb
+++ db/migrate/20200628212309_add_webpay_data_to_spree_payments.rb
@@ -1,6 +1,6 @@
 class AddWebpayDataToSpreePayments < SpreeExtension::Migration[4.2]
   def up
-    #execute 'CREATE EXTENSION IF NOT EXISTS hstore'
+    execute 'CREATE EXTENSION IF NOT EXISTS hstore'
     add_column :spree_payments, :webpay_params, :hstore
     add_column :spree_payments, :webpay_trx_id, :string
   end
@@ -8,6 +8,6 @@ class AddWebpayDataToSpreePayments < SpreeExtension::Migration[4.2]
   def down
     remove_column :spree_payments, :webpay_params
     remove_column :spree_payments, :webpay_trx_id
-    #execute 'DROP EXTENSION hstore'
+    execute 'DROP EXTENSION hstore'
   end
 end
